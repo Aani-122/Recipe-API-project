336a045f0d4d9bb45ee57087610894e376264524
Aani-122 <patil8624972776@gmail.com>
2025-02-28 10:58:12 +0530

    commands and test for them added

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,7 +2,7 @@
 calculator function
 '''
 
-def add(x,y):
+
+def add(x, y):
     '''add X and y  and return the sum'''
     return x + y
-
--- app/app/tests.py
+++ app/app/tests.py
@@ -6,13 +6,15 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
         '''test adding number together.'''
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
-        self.assertEqual(res,11)
+        self.assertEqual(res, 11)
+
     def test_another(self):
-        res = calc.add(9,0)
-
-        self.assertEqual(res,14)
+        """test adding number together. """
+        res = calc.add(9, 0)
+        self.assertEqual(res, 9)
